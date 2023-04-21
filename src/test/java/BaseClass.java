e5541558ce0101ec99a4e65641f9967b1d0412ae
Katty-Yef <tester1cat@gmail.com>
2023-04-21 19:36:54 +0300

    fixed 21.04

---
+++ src/test/java/BaseClass.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.PageFactory;
+import org.teststackoverflow.Header;
+
+import java.time.Duration;
+
+public class Base {
+    protected WebDriver driver;
+    protected Header header;
+
+    @BeforeEach
+    public void setUp(){
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(chromeOptions);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.manage().window().maximize();
+        driver.get("https://stackoverflow.com/");
+        header = PageFactory.initElements(driver, Header.class);
+    }
+
+    @AfterEach
+    public void tearDown(){
+        driver.quit();
+    }
+}
